57f017cd65254028424e75c7b908fd9c25fd24a2
Vagram_ <vdourmychev@gmail.com>
2022-03-17 10:56:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    int a;
+    public static void main(String[] args){
+        numberSquiring();
+    }
+    public static void numberSquiring(){
+        int number = new Scanner(System.in).nextInt();
+        System.out.println(number * number);
+    }
+    public static void print(){
+
+    }
+}
